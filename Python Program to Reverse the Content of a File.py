38b97bf5b40dc480ea7bc1b0b36f2c153297e1f8
Somasree Biswas <58763239+soma86@users.noreply.github.com>
2021-01-29 22:24:04 +0530

    Update Python Program to Reverse the Content of a File.py

--- Python Program to Reverse the Content of a File.py
+++ Python Program to Reverse the Content of a File.py
@@ -18,3 +18,21 @@ with open('test3_copy.txt', 'r') as rf2:
         print(i)
 
 os.remove('test3_copy.txt')
+
+
+INPUT:
+    ['i am somasree biswas\n', 'i live in pune.\n', 'Pune is in india\n', 'my # is 9881045030\n', 'eg. email is soma.biswas#123@gmail.com\n', 'welcome to my world.\n', 'i love goa.\n']
+OUTPUT:
+i love goa.
+
+welcome to my world.
+
+eg. email is soma.biswas#123@gmail.com
+
+my # is 9881045030
+
+Pune is in india
+
+i live in pune.
+
+i am somasree biswas
