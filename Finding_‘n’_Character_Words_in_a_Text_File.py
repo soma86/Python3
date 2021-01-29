479e03678e0681c05d27a0e74ac0740885406dc9
Somasree Biswas <58763239+soma86@users.noreply.github.com>
2021-01-29 16:38:33 +0530

    Create Finding_‘n’_Character_Words_in_a_Text_File.py

---
+++ Finding_‘n’_Character_Words_in_a_Text_File.py
@@ -0,0 +1,8 @@
+#Python | Finding ‘n’ Character Words in a Text File
+charlen = int(input('enter length: '))
+with open('test2.txt','r') as df:
+    nchars = df.readlines()
+    for i in nchars:
+        for j in i.split():
+            if len(j)==charlen:
+                print(j)
